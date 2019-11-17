3f3b3c99d05938f84e84f808d87f5243ce5e0a98
tingzhouu <limtingzhou@gmail.com>
2019-11-18 00:08:32 +0800

    add RNE Tooltip which does not resize

--- App.js
+++ App.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Tooltip } from 'react-native-elements';
+
+const TOOLTIP_TEXT = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vitae urna non leo maximus placerat id vel justo. Nulla in egestas risus. Ut ac sapien lorem. Sed non massa non diam sodales viverra. Curabitur non vestibulum erat, id efficitur justo.';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <View style={styles.field}>
         <View style={styles.fieldHeader}>
-          <Text>First Name</Text>
-          <Text>TOOLTIP!</Text>
+          <Text>Field Title</Text>
+          <Tooltip popover={<Text>{TOOLTIP_TEXT}</Text>}>
+          <Text>Press me</Text>
+        </Tooltip>
         </View>
         <View style={styles.fieldInput}>
-          <Text>Dummy Text</Text>
+          <Text>Dummy Input Text</Text>
+        </View>
+
+        <View style={styles.tooltipDisplaySection}>
+          <Text>Tooltip Text:</Text>
+          <Text>{TOOLTIP_TEXT}</Text>
         </View>
       </View>
     </View>
@@ -32,12 +42,18 @@ const styles = StyleSheet.create({
     width: '80%',
     flexDirection: 'row',
     justifyContent: 'space-between',
+    marginBottom: 5,
   },
   fieldInput: {
     width: '80%',
     height: 50,
     borderColor: 'red', borderWidth: 2,
     justifyContent: 'center',
+    borderRadius: 10,
+    paddingLeft: 10,
+  },
+  tooltipDisplaySection: {
+    marginTop: 20,
   },
   fieldTooltip: {
 
